9826393174d31696327c30c1fb4d2e70b65f4fb7
Youssef AbouEgla <youssefegla@gmail.com>
2020-06-22 17:08:08 +0200

    Fixed bug

--- src/store/functions/heap.ts
+++ src/store/functions/heap.ts
@@ -1,51 +1,46 @@
 export default function heapSort(a: DATA_SET) {
-  const steps: SORTING_STEPS = [[...a]];
+  const steps: SORTING_STEPS = [a];
 
   let arr = [...a];
-  console.log(arr.map((v) => v.value));
 
   let length = arr.length;
   let i = Math.floor(length / 2 - 1);
   let k = length - 1;
 
-  function heapify(arr: DATA_SET, length: number, i: number) {
-    let largest = i;
-
-    let left = i * 2 + 1;
-
-    let right = left + 1;
-
-    if (left < length && arr[left]["value"] > arr[largest]["value"]) {
-      largest = left;
-    }
-
-    if (right < length && arr[right]["value"] > arr[largest]["value"]) {
-      largest = right;
-    }
-
-    if (largest !== i) {
-      [arr[i], arr[largest]] = [arr[largest], arr[i]];
-
-      heapify(arr, length, largest);
-    }
-
-    return arr;
-  }
-
   while (i >= 0) {
-    arr = heapify(arr, length, i);
+    heapify(arr, length, i);
     i--;
   }
 
   while (k >= 0) {
-    [arr[0], arr[k]] = [arr[k], arr[0]];
-    arr = heapify(arr, k, 0);
+    swap(arr, k, 0);
+    heapify(arr, k, 0);
     k--;
   }
 
-  const final = arr.map((v) => v);
-  console.log(final);
-  steps.push(final);
-
+  steps.push(arr);
   return steps;
+}
+
+function heapify(arr: DATA_SET, length: number, i: number): void {
+  let largest = i;
+  let left = i * 2 + 1;
+  let right = left + 1;
+
+  if (left < length && arr[left]["value"] > arr[largest]["value"]) {
+    largest = left;
+  }
+
+  if (right < length && arr[right]["value"] > arr[largest]["value"]) {
+    largest = right;
+  }
+
+  if (largest !== i) {
+    swap(arr, i, largest);
+    heapify(arr, length, largest);
+  }
+}
+
+function swap(arr: DATA_SET, i: number, j: number) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
 }
