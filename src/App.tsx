447e1f9f16e2f92cecd03dd4e97e362aa1218cd3
Youssef AbouEgla <youssefegla@gmail.com>
2020-06-22 17:26:48 +0200

    Removed code explanation text

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
       </div>
       <div className={classes.bottomContainer}>
         <Container.Controls />
-        <div>Code Explaination</div>
       </div>
     </div>
   );
